24919f643bfc79675ed0c2681c41aba77aa2de9c
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-03-31 09:11:51 +0100

    connected to monoDb mongoose server

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 //create express app
 const app = express();
+
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlEncoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
+
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
+
+//configuring the database
+const dbConfig = require('./config/database.config');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+
+//connectign to database
+mongoose.connect(dbConfig.url,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+}).then(()=>{
+    console.log("Successfully connected to the database")
+}).catch(err =>{
+    console.log('Could not connect to the database. Exiting now ...', err)
+    process.exit();
+});
+
 //define a simple route
 app.get('/',(req,res,next)=>{
     res.json({"message":"Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."})
+    console.log(req.body)
 });
+
 //listen for requests
 app.listen(3030,()=>{
     console.log('Server listening on port: 3030');
